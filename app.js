6a768dbf8f967af75fd88e839ab76e4ec915923d
William Ranci <williamranci@gmail.com>
2022-11-25 20:18:28 -0500

    Added getData() function

--- app.js
+++ app.js
@@ -1,80 +1,20 @@
-$.getJSON('http://api.openweathermap.org/data/2.5/weather?q=New%20York&units=imperial&APPID=b69f44fc1c775945b18a67465501e0e9', function(data) {
-    var icon = 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png'
-    var desc = data.weather[0].main;
-    var temp = data.main.temp;
-    $('.one .desc').append(desc)
-    $('.one .icons').attr('src', icon)
-    $('.one .temp').append(temp + '&#176;' + 'F')
-});
+function getData(city, element) {
+    $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&APPID=b69f44fc1c775945b18a67465501e0e9', function(data) {
+        var icon = 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png'
+        var desc = data.weather[0].main;
+        var temp = data.main.temp;
+        $(element + ' .desc').append(desc)
+        $(element + ' .icons').attr('src', icon)
+        $(element + ' .temp').append(temp + '&#176;' + 'F')
+    });
+}
 
-$.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Los%20Angeles&units=imperial&APPID=b69f44fc1c775945b18a67465501e0e9', function(data) {
-    var icon = 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png'
-    var desc = data.weather[0].main;
-    var temp = data.main.temp;
-    $('.two .desc').append(desc)
-    $('.two .icons').attr('src', icon)
-    $('.two .temp').append(temp + '&#176;' + 'F')
-});
-
-$.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Chicago&units=imperial&APPID=b69f44fc1c775945b18a67465501e0e9', function(data) {
-    var icon = 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png'
-    var desc = data.weather[0].main;
-    var temp = data.main.temp;
-    $('.three .desc').append(desc)
-    $('.three .icons').attr('src', icon)
-    $('.three .temp').append(temp + '&#176;' + 'F')
-});
-
-$.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Houston&units=imperial&APPID=b69f44fc1c775945b18a67465501e0e9', function(data) {
-    var icon = 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png'
-    var desc = data.weather[0].main;
-    var temp = data.main.temp;
-    $('.four .desc').append(desc)
-    $('.four .icons').attr('src', icon)
-    $('.four .temp').append(temp + '&#176;' + 'F')
-});
-
-$.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Phoenix&units=imperial&APPID=b69f44fc1c775945b18a67465501e0e9', function(data) {
-    var icon = 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png'
-    var desc = data.weather[0].main;
-    var temp = data.main.temp;
-    $('.five .desc').append(desc)
-    $('.five .icons').attr('src', icon)
-    $('.five .temp').append(temp + '&#176;' + 'F')
-});
-
-$.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Philadelphia&units=imperial&APPID=b69f44fc1c775945b18a67465501e0e9', function(data) {
-    var icon = 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png'
-    var desc = data.weather[0].main;
-    var temp = data.main.temp;
-    $('.six .desc').append(desc)
-    $('.six .icons').attr('src', icon)
-    $('.six .temp').append(temp + '&#176;' + 'F')
-});
-
-$.getJSON('http://api.openweathermap.org/data/2.5/weather?q=San%20Antonio&units=imperial&APPID=b69f44fc1c775945b18a67465501e0e9', function(data) {
-    var icon = 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png'
-    var desc = data.weather[0].main;
-    var temp = data.main.temp;
-    $('.seven .desc').append(desc)
-    $('.seven .icons').attr('src', icon)
-    $('.seven .temp').append(temp + '&#176;' + 'F')
-});
-
-$.getJSON('http://api.openweathermap.org/data/2.5/weather?q=San%20Diego&units=imperial&APPID=b69f44fc1c775945b18a67465501e0e9', function(data) {
-    var icon = 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png'
-    var desc = data.weather[0].main;
-    var temp = data.main.temp;
-    $('.eight .desc').append(desc)
-    $('.eight .icons').attr('src', icon)
-    $('.eight .temp').append(temp + '&#176;' + 'F')
-});
-
-$.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Dallas&units=imperial&APPID=b69f44fc1c775945b18a67465501e0e9', function(data) {
-    var icon = 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png'
-    var desc = data.weather[0].main;
-    var temp = data.main.temp;
-    $('.nine .desc').append(desc)
-    $('.nine .icons').attr('src', icon)
-    $('.nine .temp').append(temp + '&#176;' + 'F')
-});
+getData('New York', '.one')
+getData('Los Angeles', '.two')
+getData('Chicago', '.three')
+getData('Houston', '.four')
+getData('Phoenix', '.five')
+getData('Philadelphia', '.six')
+getData('San Antonio', '.seven')
+getData('San Diego', '.eight')
+getData('Dallas', '.nine')
