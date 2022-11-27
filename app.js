35e1446b4e25d74f9518c7a8256fb765e6da0596
William Ranci <williamranci@gmail.com>
2022-11-27 16:45:43 -0500

    Display getMoreData() on modal

--- app.js
+++ app.js
@@ -9,6 +9,31 @@ function getData(city, element) {
     });
 }
 
+function getMoreData(city, element) {
+  $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&APPID=b69f44fc1c775945b18a67465501e0e9', function(data) {
+    var icon = 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png'
+    var desc = data.weather[0].description
+    var temp = data.main.temp
+    var feelsLike = data.main.feels_like
+    var tempMin = data.main.min_temp
+    var tempMax = data.main.max_temp
+    var pressure = data.main.pressure
+    var humidity = data.main.humidity
+    var windSpeed = data.wind.speed
+
+    $(element + ' .name').append(city)
+    $(element + ' .desc').append('Description: ' + desc)
+    $(element + ' .icons').attr('src', icon)
+    $(element + ' .temp').append('Temperature: ' + temp + '&#176;' + 'F')
+    $(element + ' .feels-like').append('Feels Like: ' + feelsLike + '&#176;' + 'F')
+    $(element + ' .temp-min').append(tempMin)
+    $(element + ' .temp-max').append(tempMax)
+    $(element + ' .pressure').append('Pressure: ' + pressure + ' hPa')
+    $(element + ' .humidity').append('Humidity: ' + humidity + '%')
+    $(element + ' .wind-speed').append('Wind Speed: ' + windSpeed + ' mi.')
+  });
+}
+
 getData('New York', '.one')
 getData('Los Angeles', '.two')
 getData('Chicago', '.three')
@@ -18,6 +43,8 @@ getData('Philadelphia', '.six')
 getData('San Antonio', '.seven')
 getData('San Diego', '.eight')
 getData('Dallas', '.nine')
+
+getMoreData('New York', '.modal-data')
 
 // Get the modal
 var modal = document.getElementById("myModal");
